db3a3417e3e408530756bf61e71a991f8a088c15
Aneesh kumar maury <aneeshmaury.in@gmail.com>
2024-02-22 19:26:55 +0530

    jhj

--- app/page.js
+++ app/page.js
@@ -5,14 +5,14 @@ export default function Home() {
   return (
    <main>
   <Navbar/>
-  <div className="w-200 h-44 overflow-y-auto bg-gray-200 flex flex-wrap mt-3 pt-11">
-  <ul className="list-none text-center justify-center ml-[30px]">
-    <li className="bg-slate-800 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
-    <li className="bg-slate-800 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
-    <li className="bg-slate-800 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
-    <li className="bg-slate-800 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
-    <li className="bg-slate-800 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
-    <li className="bg-slate-800 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
+  <div className="w-200 h-44 overflow-y-auto bg-gray-200 flex justify-center flex-wrap mt-3 pt-11">
+  <ul className="list-none  ">
+    <li className="bg-slate-800 mb-1 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
+    <li className="bg-slate-800 mb-1 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
+    <li className="bg-slate-800 mb-1 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
+    <li className="bg-slate-800 mb-1 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
+    <li className="bg-slate-800 mb-1 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
+    <li className="bg-slate-800 mb-1 text-white rounded-sm" style={{ width: '320px', wordWrap: 'break-word', padding: '1px' }}>SSC Constable GD in Central Armed Police Forces</li>
   </ul>
 </div>
 
